d8b3e3a1d64353f1f3c69bea12945857a453aa58
MD Tajkir Alam Rion <tajkir.alam@gmail.com>
2024-04-21 18:16:30 +0600

    signin page layout

--- resources/js/Pages/AccessControl/SignIn/SignIn.jsx
+++ resources/js/Pages/AccessControl/SignIn/SignIn.jsx
@@ -3,10 +3,15 @@ import Header from '../../../Component/Header/Header';
 
 const SignIn = () => {
     return (
-        <div>
+        <>
             <Header></Header>
-            signin
-        </div>
+            <div className='font-sora bg-[#FAFAFA] flex justify-center min-h-screen py-16'>
+                <form className='bg-white shadow-lg w-2/5 px-5 py-8'>
+                    <h4 className='text-[26px] mb-10'>Log In</h4>
+                    <label htmlFor="email" className='text-sm'>Email Address</label>
+                </form>
+            </div>
+        </>
     );
 };
 
