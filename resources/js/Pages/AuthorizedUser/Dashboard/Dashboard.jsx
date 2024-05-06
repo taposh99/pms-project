e0ae5227c25f56f431e21dea378e45507450704e
MD Tajkir Alam Rion <tajkir.alam@gmail.com>
2024-05-06 12:02:57 +0600

    dashboard/summery analytics done

--- resources/js/Pages/AuthorizedUser/Dashboard/Dashboard.jsx
+++ resources/js/Pages/AuthorizedUser/Dashboard/Dashboard.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { FaPlus } from "react-icons/fa6";
+import SummeryAnalytics from './SummeryAnalytics';
 
 const Dashboard = () => {
     return (
         <main className='px-5 py-3'>
             <p className='font-sora font-semibold'>Dashboard</p>
             <div className='flex justify-end'>
-                <button className='btn flex items-center gap-2 bg-[#04DA8D] text-white px-5 py-2 rounded-3xl'>
+                <button className='btn flex items-center gap-2 bg-[#04DA8D] font-sora font-semibold tracking-wider text-sm text-white px-5 py-3 rounded-3xl'>
                     <FaPlus />
                     ADD WIDGET
                 </button>
             </div>
+            <SummeryAnalytics />
         </main>
     );
 };
